0100bbc027157b3be97fb6adde393002a251e2df
André Maree <andrem@kss.co.za>
2018-12-19 11:12:27 +0200

    Initial commit

---
+++ x_sntp_client.c
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2014-18 Andre M Maree / KSS Technologies (Pty) Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*
+ * x_sntp_client.c
+ */
+
+#include	"x_debug.h"
+#include 	"x_sntp_client.h"
+#include	"x_sockets.h"
+#include	"x_errors_events.h"
+#include	"x_syslog.h"
+
+#include	<limits.h>
+#include	<string.h>
+#include	<math.h>
+
+// ###################################### DEBUG : CONFIG definitions ###############################
+
+#define	debugFLAG						0x0000
+#define	debugPROTOCOL					(debugFLAG * 0x0001)
+#define	debugHOSTS						(debugFLAG * 0x0002)
+#define	debugCALCULATION				(debugFLAG * 0x0004)
+
+// ############################################ Macros  ############################################
+
+#define	STRATUM_IDX(x)	((x >= specNTP_STRATUM_RSVD_LO) ? 4 : (x == specNTP_STRATUM_UNSYNC)	? 3 : (x >= specNTP_STRATUM_SEC_LO)	? 2 : x )
+
+// ###################################### local ie static variables ################################
+
+ntp_t		sNtpBuf ;
+int16_t		NtpHostIndex = 0 ;
+uint64_t	tNTP[4] ;
+int64_t		tRTD, tOFF ;
+
+const char * NtpHostTable[] = {
+	"ntp1.meraka.csir.co.za",
+	"ntp1.neology.co.za",
+	"ntp2.meraka.csir.co.za",
+	"ntp2.neology.co.za",
+	"0.za.pool.ntp.org",
+	"1.za.pool.ntp.org",
+	"2.za.pool.ntp.org",
+	"3.za.pool.ntp.org",
+} ;
+
+#define		NTP_TABLE_SIZE			( sizeof(NtpHostTable) / sizeof(NtpHostTable[0]) )
+
+/*
+ * xNTPCalcValue() convert NTP epoch NETWORK seconds/fractions to UNIX epoch HOST microseconds
+ */
+uint64_t xNTPCalcValue(uint32_t Secs, uint32_t Frac) {
+uint64_t	u64Val1, u64Val2 ;
+	u64Val1	= ntohl(Secs) - EPOCH_SECONDS_DIFFERENCE ;	// difference between NTP and selected epoch
+	u64Val1 *= MICROS_IN_SECOND ;						// convert Secs to uSec
+	u64Val2	= ntohl(Frac) / FRACTIONS_PER_MICROSEC ;	// convert Frac to uSec
+	return u64Val1 + u64Val2 ;
+}
+
+/*
+ * vNtpDebug() - Display the sNtpBuf header info as from server
+ */
+void	vNtpDebug(void) {
+static const char *	LI_mess[]	= { "None", "61Sec", "59Sec", "Alarm" } ;
+static const char *	Mode_mess[]	= { "Unspec", "SymAct", "SymPas", "Client", "Server", "BCast", "RsvdNTP", "RsvdPriv" } ;
+static const char *	Strat_mess[]= { "KofD", "Prim", "Sec", "UnSync" , "Rsvd" } ;
+// Display the header info
+	PRINT("[NTP] LI[%s] V[%u] Mode[%s] Stratum[%s] Poll[%.1fs] Precision[%fuS]\n",
+											LI_mess[sNtpBuf.LI],
+											sNtpBuf.VN,
+											Mode_mess[sNtpBuf.Mode],
+											Strat_mess[STRATUM_IDX(sNtpBuf.Stratum)],
+											pow(2, (double) sNtpBuf.Poll),
+											pow(2, (double) sNtpBuf.Precision) * 1000000) ;
+	PRINT("[NTP] Root Delay[%d.%04d Sec]\n",
+											ntohs(sNtpBuf.RDelUnit),
+											ntohs(sNtpBuf.RDelFrac) / (UINT16_MAX / 10000) ) ;
+	PRINT("[NTP] Dispersion[%u.%04u Sec]\n",
+											ntohs(sNtpBuf.RDisUnit),
+											ntohs(sNtpBuf.RDisFrac) / (UINT16_MAX / 10000) ) ;
+	if (sNtpBuf.Stratum <= specNTP_STRATUM_PRI) {
+		PRINT("[NTP] Ref ID[%4s]\n", &sNtpBuf.RefID) ;
+	} else {
+		PRINT("[NTP] Ref IP[%-I]\n", ntohl(sNtpBuf.RefIP)) ;
+	}
+
+// determine and display the reference timestamp
+	uint64_t tTemp	= xNTPCalcValue(sNtpBuf.Ref.secs, sNtpBuf.Ref.frac) ;
+	static TSZ_t tt ;
+	tt.usecs = tTemp ;
+	PRINT("[NTP] Ref: %Z\n", &tt) ;
+// Display the 4 different timestamps
+	tt.usecs = tNTP[0] ;
+	PRINT("[NTP] (t0) %Z\n", &tt) ;
+	tt.usecs = tNTP[1] ;
+	PRINT("[NTP] (t1) %Z\n", &tt) ;
+	tt.usecs = tNTP[2] ;
+	PRINT("[NTP] (t2) %Z\n", &tt) ;
+	tt.usecs = tNTP[3] ;
+	PRINT("[NTP] (t3) %Z\n", &tt) ;
+}
+
+/*
+ * vNtpCalcCorrectTime()
+ */
+void	vNtpCalcCorrectTime(uint64_t * pTStamp) {
+int64_t		tT0, tT1 ;
+	tNTP[0]	= xNTPCalcValue(sNtpBuf.Orig.secs , sNtpBuf.Orig.frac) ;
+	tNTP[1]	= xNTPCalcValue(sNtpBuf.Recv.secs , sNtpBuf.Recv.frac) ;
+	tNTP[2]	= xNTPCalcValue(sNtpBuf.Xmit.secs , sNtpBuf.Xmit.frac) ;
+	tNTP[3] = *pTStamp ;
+// Calculate the Round Trip Delay
+	tT0		= tNTP[3] - tNTP[0] ;
+	tT1		= tNTP[2] - tNTP[1] ;
+	tRTD	= tT0 - tT1 ;
+// Then do the Offset in steps...
+	tT0		= tNTP[1] - tNTP[0] ;
+	tT1		= tNTP[2] - tNTP[3] ;
+	tOFF	= tT0 + tT1 ;
+	tOFF	/= 2 ;
+	*pTStamp = tNTP[0] + tRTD + tOFF ;
+	IF_SL_DBG(debugCALCULATION, "'%s' %R tOFF=%'lld uS tRTD=%'lld uS", NtpHostTable[NtpHostIndex], *pTStamp, tOFF, tRTD) ;
+}
+
+int32_t	xNtpRequestInfo(sock_ctx_t * psNtpCtx, uint64_t * pTStamp) {
+	memset(&sNtpBuf, 0, sizeof(ntp_t)) ;
+	sNtpBuf.VN			= specNTP_VERSION_V4 ;
+	sNtpBuf.Mode		= specNTP_MODE_CLIENT ;
+
+	// Plug in current (possibly very wrong) client transmit time
+	sNtpBuf.Xmit.secs 	= htonl(xTimeStampAsSeconds(*pTStamp) + EPOCH_SECONDS_DIFFERENCE) ;
+	sNtpBuf.Xmit.frac	= htonl((*pTStamp % MICROS_IN_SECOND) * FRACTIONS_PER_MICROSEC) ;
+
+	// send the formatted request
+	int32_t iRetVal = xNetWrite(psNtpCtx, (char *) &sNtpBuf, sizeof(ntp_t)) ;
+	if (iRetVal == sizeof(ntp_t)) {
+		xNetSetRecvTimeOut(psNtpCtx, 400) ;
+		iRetVal = xNetRead(psNtpCtx, (char *) &sNtpBuf, sizeof(ntp_t)) ;
+	}
+	if (iRetVal != sizeof(ntp_t)) {
+		return iRetVal ;
+	}
+	// expect only server type responses with correct version and stratum
+	if (sNtpBuf.Mode != specNTP_MODE_SERVER ||
+		sNtpBuf.VN != specNTP_VERSION_V4	||
+   		OUTSIDE(specNTP_STRATUM_PRI, sNtpBuf.Stratum, specNTP_STRATUM_SEC_HI, uint8_t)) {
+		SL_ERR("Mode=%d  Ver=%d  Stratum=%d", sNtpBuf.Mode, sNtpBuf.VN, sNtpBuf.Stratum) ;
+   		return erFAILURE ;
+   	}
+	IF_PRINT(debugHOSTS, "Sync'ing with host %s\n", NtpHostTable[NtpHostIndex]) ;
+	return iRetVal ;
+}
+
+/*
+ *! xNetNTPGetTime() - Gets the current time from the selected (S)sNtpBuf server
+ *! \brief  	This function obtains the sNtpBuf time from the server.
+ *! \param[in]	hostname - name of sNtpBuf host to query
+ *! \param[out]	pSystime - pointer to system time structure
+ *! \return		NEGATIVE - socket or application error code
+ *				ZERO	 -	if all OK
+ */
+int32_t xNtpGetTime(uint64_t * pTStamp) {
+	sock_ctx_t	sNtpCtx ;
+	memset(&sNtpCtx, 0, sizeof(sock_ctx_t)) ;
+	sNtpCtx.sa_in.sin_family	= AF_INET ;
+	sNtpCtx.sa_in.sin_port		= htons(IP_PORT_NTP) ;
+	sNtpCtx.type				= SOCK_DGRAM ;
+#if 0
+	sNtpCtx.d_open				= 1 ;
+	sNtpCtx.d_write				= 1 ;
+	sNtpCtx.d_read				= 1 ;
+#endif
+	for (int32_t iRetVal = -1; iRetVal != sizeof(ntp_t) ; ) {
+		sNtpCtx.pHost	= NtpHostTable[NtpHostIndex] ;
+		IF_PRINT(debugHOSTS, "Connecting to host %s\n", sNtpCtx.pHost) ;
+		iRetVal = xNetOpen(&sNtpCtx) ;
+		if (iRetVal >= erSUCCESS) {
+			iRetVal = xNtpRequestInfo(&sNtpCtx, pTStamp) ;// send the sNtpBuf request & check the result
+		}
+		xNetClose(&sNtpCtx) ;							// close, & ignore return code..
+		if (iRetVal != sizeof(ntp_t)) {
+			vTaskDelay(pdMS_TO_TICKS(1000)) ;			// wait 1 seconds
+			++NtpHostIndex ;
+			NtpHostIndex %= NTP_TABLE_SIZE ;			// failed, step to next host...
+		}
+	}
+
+	// end of loop, must have a valid HOST
+	vNtpCalcCorrectTime(pTStamp) ;						// calculate & update correct time
+	IF_EXEC_0(debugPROTOCOL, vNtpDebug) ;
+	return erSUCCESS ;
+}
+
+/*
+ * vSntpTask()
+ */
+void	vSntpTask(void * pvPara) {
+	IF_TRACK(debugAPPL_THREADS, messageTASK_START) ;
+	TickType_t	NtpLWtime = xTaskGetTickCount();			// Get the current time as a reference to start our delays.
+	while (xRtosVerifyState(taskSNTP)) {
+		vRtosWaitStatus(flagNET_L3) ;					// first wait till IP is up and running
+		if (xNtpGetTime((uint64_t *) pvPara) == erSUCCESS) {
+#if		defined(cc3200)
+			halWL_SetDateTime(CurSecs.unit) ;				// setup correct time in NWP
+#endif
+			vRtosSetStatus(flagNET_SNTP) ;
+			SL_INFO("%s  %R  tOFF=%'llduS  tRTD=%'llduS", NtpHostTable[NtpHostIndex], (uint64_t) * (uint64_t *) pvPara, tOFF, tRTD) ;
+		} else {
+			SL_ERR("updating time") ;
+		}
+		vTaskDelayUntil(&NtpLWtime, pdMS_TO_TICKS(sntpINTERVAL_MS)) ;
+	}
+	IF_TRACK(debugAPPL_THREADS, messageTASK_DELETE) ;
+	vRtosClearStatus(flagNET_SNTP) ;
+	vTaskDelete(NULL) ;
+}
+
+void	vTaskSntpInit(uint64_t * pTStamp) { xRtosTaskCreate(vSntpTask, "SNTP", sntpSTACK_SIZE, pTStamp, 2, NULL, INT_MAX) ; }
---
+++ x_sntp_client.h
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2014-18 Andre M Maree / KSS Technologies (Pty) Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*
+ * x_sntp_client.h
+ */
+
+#pragma once
+
+#include	"x_definitions.h"				// bring x_time.h with..
+#include	"FreeRTOS_Support.h"			// needed for configMINIMAL_STACK_SIZE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ###################################### BUILD : CONFIG definitions ###############################
+
+#define	sntpPRIORITY						1
+#define sntpINTERVAL_MS						(60 * SECONDS_IN_MINUTE * MILLIS_IN_SECOND)
+#define	sntpSTACK_SIZE						(configMINIMAL_STACK_SIZE + 1280)
+
+// #################################### NTP packet structures ######################################
+
+enum {
+	specNTP_VERSION_V1				= 1,
+	specNTP_VERSION_V2				= 2,
+	specNTP_VERSION_V3				= 3,
+	specNTP_VERSION_V4				= 4,
+} ;
+
+enum {
+	specNTP_MODE_RESERVED			= 0,
+	specNTP_MODE_SYM_ACTV			= 1,
+	specNTP_MODE_SYM_PASV			= 2,
+	specNTP_MODE_CLIENT				= 3,
+	specNTP_MODE_SERVER				= 4,
+	specNTP_MODE_BROADCAST			= 5,
+	specNTP_MODE_NTP_CONTROL		= 6,
+	specNTP_MODE_PRIVATE			= 7,
+} ;
+
+enum {
+	specNTP_STRATUM_KISS_O_DEATH	= 0,
+	specNTP_STRATUM_PRI				= 1,
+	specNTP_STRATUM_SEC_LO			= 2,
+	specNTP_STRATUM_SEC_HI			= 15,
+	specNTP_STRATUM_UNSYNC			= 16,
+	specNTP_STRATUM_RSVD_LO			= 17,
+	specNTP_STRATUM_RESERVED_HI		= 255,
+} ;
+
+typedef union {
+	uint64_t	val ;
+	struct {
+		uint32_t	secs ;
+		uint32_t	frac ;
+	} ;
+} ntp_ts_t ;
+
+/************************ NTP packet structure **********************
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |MSb		 LSb|MSb         LSb|MSb         LSb|MSb         LSb|
+ 	+-+-+-+-+-+-+-+-+-+-+-+- NTP Packet Header -+-+-+-+-+-+-+-+-+-+-+
+    |0 1|2 3 4|5 6 7|8 9 0 1 2 3 4 5|6 7 8 9 0 1 2 3|4 5 6 7 8 9 0 1|
+ 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x00|L I| V N |Mode |    Stratum    |     Poll      |   Precision   |
+   	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x04|                          Root Delay                           |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x08|                        Root Dispersion                        |
+  	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x0C|                     Reference Identifier                      |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x10|                    Reference Timestamp (64)                   |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x18|                    Originate Timestamp (64)                   |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x20|                     Receive Timestamp (64)                    |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x28|                     Transmit Timestamp (64)                   |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x30|                 Key Identifier (optional) (32)                |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+0x34|                 Message Digest (optional) (128)               |
+	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    Getting the data from the Transmit Timestamp (seconds) field
+This is the time at which the reply departed the server for the client
+	https://www.meinbergglobal.com/english/info/ntp-packet.htm
+	https://tools.ietf.org/html/rfc5905									*/
+
+typedef	struct __attribute__((__packed__)) ntp_s {
+	struct	{									// Header fields
+		uint8_t		Mode	: 3 ;				// Mode (0=resv,active,passive,client,server,broadcast,control,rsvd)
+		uint8_t		VN		: 3 ;				// Version (currently 4)
+		uint8_t		LI		: 2 ;				// Leap Indicator (0=no warning,1=LastMin=61,2=LastMin=59,3=unknown)
+	} ;
+	uint8_t		Stratum ;						// Stratum (0=unspec,1=pri,2-15=secondary,16=unsync,17-255=rsvd)
+	uint8_t		Poll ;							// Poll Exponent (max intvl between mess in log2 sec, range 4(16s) -> 17(131072s))
+	int8_t		Precision ;						// Precision of system clock in log2 sec (-18 = 1uSec)
+	struct {									// 0x04: Root Delay
+		int16_t		RDelUnit ;
+		uint16_t	RDelFrac ;
+	} ;
+	struct {									// 0x08: Root Dispersion
+		uint16_t	RDisUnit ;
+		uint16_t	RDisFrac ;
+	} ;
+	union {										// 0x0C: Reference ID or IP
+		uint8_t		RefID[4] ;
+		uint32_t	RefIP ;
+	} ;
+	ntp_ts_t	Ref ;
+	ntp_ts_t	Orig ;
+	ntp_ts_t	Recv ;
+	ntp_ts_t	Xmit ;
+//	Only used with authorisation ...
+//	uint32_t	KeyID ;
+//	uint32_t	MessDigest[4] ;
+} ntp_t ;
+
+// ################################### Global variables ############################################
+
+
+// ############################### Level 2 network functions #######################################
+
+int32_t xNtpGetTime(uint64_t * pTStamp) ;
+void	vSntpTask(void * pvPara) ;
+void	vTaskSntpInit(uint64_t * pTStamp) ;
+
+#ifdef __cplusplus
+}
+#endif
