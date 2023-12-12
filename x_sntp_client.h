239b8e103f7f09a17cf76748cb67c22a943ba835
André Maree <ammaree@users.noreply.github.com>
2023-12-12 18:49:32 +0200

    Update x_sntp_client.h

--- x_sntp_client.h
+++ x_sntp_client.h
@@ -1,6 +1,5 @@
 /*
- * Copyright 2014-21 Andre M. Maree / KSS Technologies (Pty) Ltd.
- * x_sntp_client.h
+ * x_sntp_client.h - Copyright (c) 2014-23 Andre M. Maree / KSS Technologies (Pty) Ltd.
  */
 
 #pragma once
@@ -22,7 +21,7 @@ enum {
 	specNTP_VERSION_V2				= 2,
 	specNTP_VERSION_V3				= 3,
 	specNTP_VERSION_V4				= 4,
-} ;
+};
 
 enum {
 	specNTP_MODE_RESERVED			= 0,
@@ -33,7 +32,7 @@ enum {
 	specNTP_MODE_BROADCAST			= 5,
 	specNTP_MODE_NTP_CONTROL		= 6,
 	specNTP_MODE_PRIVATE			= 7,
-} ;
+};
 
 enum {
 	specNTP_STRATUM_KISS_O_DEATH	= 0,
@@ -43,17 +42,17 @@ enum {
 	specNTP_STRATUM_UNSYNC			= 16,
 	specNTP_STRATUM_RSVD_LO			= 17,
 	specNTP_STRATUM_RESERVED_HI		= 255,
-} ;
+};
 
 // #################################### NTP packet structures ######################################
 
 typedef union {
-	uint64_t	val ;
+	u64_t val;
 	struct {
-		uint32_t	secs ;
-		uint32_t	frac ;
-	} ;
-} ntp_ts_t ;
+		u32_t secs;
+		u32_t frac;
+	};
+} ntp_ts_t;
 
 /************************ NTP packet structure **********************
 	+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -87,40 +86,40 @@ This is the time at which the reply departed the server for the client
 	https://tools.ietf.org/html/rfc5905									*/
 
 typedef	struct __attribute__((__packed__)) {
-	struct	{									// Header fields
-		uint8_t		Mode	: 3 ;				// Mode (0=resv,active,passive,client,server,broadcast,control,rsvd)
-		uint8_t		VN		: 3 ;				// Version (currently 4)
-		uint8_t		LI		: 2 ;				// Leap Indicator (0=no warning,1=LastMin=61,2=LastMin=59,3=unknown)
-	} ;
-	uint8_t		Stratum ;						// Stratum (0=unspec,1=pri,2-15=secondary,16=unsync,17-255=rsvd)
-	uint8_t		Poll ;							// Poll Exponent (max intvl between mess in log2 sec, range 4(16s) -> 17(131072s))
-	int8_t		Precision ;						// Precision of system clock in log2 sec (-18 = 1uSec)
-	struct {									// 0x04: Root Delay
-		int16_t		RDelUnit ;
-		uint16_t	RDelFrac ;
-	} ;
-	struct {									// 0x08: Root Dispersion
-		uint16_t	RDisUnit ;
-		uint16_t	RDisFrac ;
-	} ;
-	union {										// 0x0C: Reference ID or IP
-		uint8_t		RefID[4] ;
-		uint32_t	RefIP ;
-	} ;
-	ntp_ts_t	Ref ;
-	ntp_ts_t	Orig ;
-	ntp_ts_t	Recv ;
-	ntp_ts_t	Xmit ;
+	struct	{						// Header fields
+		u8_t Mode: 3;				// Mode (0=resv,active,passive,client,server,broadcast,control,rsvd)
+		u8_t VN: 3;					// Version (currently 4)
+		u8_t LI: 2;					// Leap Indicator (0=no warning,1=LastMin=61,2=LastMin=59,3=unknown)
+	};
+	u8_t Stratum;					// Stratum (0=unspec,1=pri,2-15=secondary,16=unsync,17-255=rsvd)
+	u8_t Poll;						// Poll Exponent (max intvl between mess in log2 sec, range 4(16s) -> 17(131072s))
+	i8_t Precision;					// Precision of system clock in log2 sec (-18 = 1uSec)
+	struct {						// 0x04: Root Delay
+		i16_t RDelUnit;
+		u16_t RDelFrac;
+	};
+	struct {						// 0x08: Root Dispersion
+		u16_t RDisUnit;
+		u16_t RDisFrac;
+	};
+	union {							// 0x0C: Reference ID or IP
+		u8_t RefID[4];
+		u32_t RefIP;
+	};
+	ntp_ts_t Ref;
+	ntp_ts_t Orig;
+	ntp_ts_t Recv;
+	ntp_ts_t Xmit;
 //	Only used with authorisation ...
-//	uint32_t	KeyID ;
-//	uint32_t	MessDigest[4] ;
-} ntp_t ;
+//	u32_t	KeyID;
+//	u32_t	MessDigest[4];
+} ntp_t;
 
 // ################################### Global variables ############################################
 
 // ############################### Level 2 network functions #######################################
 
-int xNtpGetTime(uint64_t *);
+int xNtpGetTime(u64_t *);
 void vSntpStart(void *);
 
 #ifdef __cplusplus
