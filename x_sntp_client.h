c7680ed8fb49cebf5839489aaeef09156a9779e2
André Maree <ammaree@users.noreply.github.com>
2024-02-06 23:02:13 +0200

    Update copyright year

--- x_sntp_client.h
+++ x_sntp_client.h
@@ -1,5 +1,5 @@
 /*
- * x_sntp_client.h - Copyright (c) 2014-23 Andre M. Maree / KSS Technologies (Pty) Ltd.
+ * x_sntp_client.h - Copyright (c) 2014-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
  */
 
 #pragma once
